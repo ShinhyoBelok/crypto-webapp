b2bb1d31acd6d7c401d693800ff849ec255c5ea1
shinhyo26okazaki <shibo6937@gmail.com>
2023-02-08 15:19:29 -0400

    Add hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import '../css/Hero.css';
 
 export default function Hero() {
   return (
-    <div>Hero</div>
+    <section className="hero flex">
+      <img src="https://cdn-icons-png.flaticon.com/512/3461/3461335.png" alt="Trading Graph" />
+      <h2>100 CRYPTOS</h2>
+    </section>
   );
 }
